44183d61c06c2f7aa5ac1a1b31e44b9b01b6f212
Diego Alves <ifdiego@users.noreply.github.com>
2025-05-20 21:37:29 -0300

    feat: add bst implementation

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ different programming languages.
 
 If you find something wrong here or wants to contribute with a suggestion or
 some code, feel free to fork and open a PR.
+
+[femasters]: https://frontendmasters.com/courses/algorithms
+[leetcode]: https://leetcode.com/u/ifdiego
+[repo]: https://github.com/ThePrimeagen/fem-algos
+[slides]: https://theprimeagen.github.io/fem-algos
---
+++ data-structures/tree/binary_search_tree.ts
@@ -0,0 +1,56 @@
+class Node {
+  data: number;
+  left: Node | null;
+  right: Node | null;
+
+  constructor(data: number) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+}
+
+function insert(root: Node | null, value: number): Node {
+  if (root === null) {
+    return new Node(value);
+  }
+  if (root.data === value) {
+    return root;
+  }
+  if (value < root.data) {
+    root.left = insert(root.left, value);
+  } else {
+    root.right = insert(root.right, value);
+  }
+  return root;
+}
+
+function inOrderTraversal(root: Node | null): void {
+  if (root !== null) {
+    inOrderTraversal(root.left);
+    console.log(root.data);
+    inOrderTraversal(root.right);
+  }
+}
+
+function search(root: Node | null, value: number): Node | null {
+  if (root === null || root.data === value) {
+    return root;
+  }
+  if (value < root.data) {
+    return search(root.left, value);
+  } else {
+    return search(root.right, value);
+  }
+}
+
+function main(): void {
+  let tree: Node | null = null;
+  tree = insert(tree, 42);
+  tree = insert(tree, 8);
+  tree = insert(tree, 99);
+  tree = insert(tree, 31);
+  inOrderTraversal(tree);
+}
+
+main();
